af4896021f8e06e9479a39690d047e55bd1295cc
Matias S <mschertel@gmail.com>
2020-04-12 19:08:07 -0300

    to imutable objects

--- entrypoints/src/main/java/dev/schertel/cq/circular/CircularQueueDto.java
+++ entrypoints/src/main/java/dev/schertel/cq/circular/CircularQueueDto.java
@@ -29,39 +29,19 @@ public class CircularQueueDto {
         return id;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
