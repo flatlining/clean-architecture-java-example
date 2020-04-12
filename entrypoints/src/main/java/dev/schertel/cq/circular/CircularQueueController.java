12a3c957aba768f8d2463a5252178d65b46499b2
Matias S <mschertel@gmail.com>
2020-04-12 18:31:14 -0300

    Fix dependencies

--- entrypoints/src/main/java/dev/schertel/cq/circular/CircularQueueController.java
+++ entrypoints/src/main/java/dev/schertel/cq/circular/CircularQueueController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/circular")
 public class CircularQueueController {
     @GetMapping
     private Flux<CircularQueueDto> getAllCircularQueues() {
-        CircularQueueDto cq1 = new CircularQueueDto();
-        CircularQueueDto cq2 = new CircularQueueDto();
+        CircularQueueDto cq1 = new CircularQueueDto(UUID.randomUUID(), "Queue 1", "Circular queue 1", LocalDateTime.now(), LocalDateTime.now());
+        CircularQueueDto cq2 = new CircularQueueDto(UUID.randomUUID(), "Queue 2", "Circular queue 2", LocalDateTime.now(), LocalDateTime.now());
         return (Flux<CircularQueueDto>) Flux.just(cq1, cq2);
     }
 }
