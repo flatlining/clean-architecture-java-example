70625a3a81fc2d97730c57d45e5e181d824a8bce
Matias S <mschertel@gmail.com>
2020-04-26 12:23:38 -0300

    add tests to Identity

---
+++ core/src/test/java/dev/schertel/cq/core/domain/IdentityTest.java
@@ -0,0 +1,86 @@
+package dev.schertel.cq.core.domain;
+
+import com.jparams.verifier.tostring.NameStyle;
+import com.jparams.verifier.tostring.ToStringVerifier;
+import io.github.glytching.junit.extension.random.Random;
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(RandomBeansExtension.class)
+class IdentityTest {
+    Identity.IdentityBuilder builder;
+    Identity cut;
+
+    @BeforeEach
+    void setUp() {
+        this.builder = Identity.builder();
+        this.cut = null;
+    }
+
+    @Test
+    void of(@Random String id) {
+        cut = Identity.of(id);
+
+        assertAll(
+                () -> assertEquals(id, cut.getId())
+        );
+    }
+
+    @Test
+    void getId(@Random String id) {
+        cut = builder
+                .withId(id)
+                .build();
+
+        assertAll(
+                () -> assertEquals(id, cut.getId())
+        );
+    }
+
+    @Nested
+    class Builder {
+        @Test
+        void nullObject() {
+            cut = builder.build();
+
+            assertAll(
+                    () -> assertNull(cut.getId())
+            );
+        }
+
+        @Test
+        void fullObject(@Random String id) {
+            cut = builder
+                    .withId(id)
+                    .build();
+
+            assertAll(
+                    () -> assertEquals(id, cut.getId())
+            );
+        }
+    }
+
+    @Nested
+    class Override {
+        private final Class<?> CLAZZ = Identity.class;
+
+        @Test
+        void testToString() {
+            ToStringVerifier.forClass(CLAZZ)
+                    .withClassName(NameStyle.SIMPLE_NAME)
+                    .verify();
+        }
+
+        @Test
+        void testEquals() {
+            EqualsVerifier.forClass(CLAZZ)
+                    .verify();
+        }
+    }
+}
