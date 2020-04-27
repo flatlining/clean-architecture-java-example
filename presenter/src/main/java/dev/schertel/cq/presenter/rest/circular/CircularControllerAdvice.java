8f477fb114a5e74d3d2d47f49c099085c0154f6e
Matias S <mschertel@gmail.com>
2020-04-26 21:39:57 -0300

    add tests

--- presenter/src/main/java/dev/schertel/cq/presenter/rest/circular/CircularControllerAdvice.java
+++ presenter/src/main/java/dev/schertel/cq/presenter/rest/circular/CircularControllerAdvice.java
@@ -1,12 +1,12 @@
 package dev.schertel.cq.presenter.rest.circular;
 
+import dev.schertel.cq.core.domain.DomainException;
 import dev.schertel.cq.core.domain.NotFoundException;
 import dev.schertel.cq.presenter.rest.entity.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZoneOffset;
@@ -15,8 +15,20 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class CircularControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiResponse> customHandleNotFound(NotFoundException ex, WebRequest request) {
+    public ResponseEntity<ApiResponse> customNotFoundException(NotFoundException ex) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        ApiResponse apiResponse = ApiResponse.builder()
+                .withTimestamp(ZonedDateTime.now(ZoneOffset.UTC))
+                .withStatus(httpStatus.value())
+                .withReason(httpStatus.getReasonPhrase())
+                .withMessage(ex.getMessage())
+                .build();
+        return new ResponseEntity<ApiResponse>(apiResponse, httpStatus);
+    }
+
+    @ExceptionHandler(value = {DomainException.class})
+    public ResponseEntity<ApiResponse> customDomainException(DomainException ex) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         ApiResponse apiResponse = ApiResponse.builder()
                 .withTimestamp(ZonedDateTime.now(ZoneOffset.UTC))
                 .withStatus(httpStatus.value())
