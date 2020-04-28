6a5fba911fbace6ed447e28102de9ff9f8ad1063
Matias S <mschertel@gmail.com>
2020-04-27 22:20:15 -0300

    finish create test

--- presenter/src/test/java/dev/schertel/cq/presenter/rest/circular/CircularControllerTest.java
+++ presenter/src/test/java/dev/schertel/cq/presenter/rest/circular/CircularControllerTest.java
@@ -7,6 +7,7 @@ import dev.schertel.cq.core.usecase.circular.*;
 import dev.schertel.cq.presenter.rest.entity.CircularRequest;
 import dev.schertel.cq.presenter.rest.entity.CircularResponse;
 import dev.schertel.cq.presenter.usecase.UseCaseExecutorImpl;
+import io.github.glytching.junit.extension.random.Random;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,17 +18,19 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RandomBeansExtension.class)
 @WebMvcTest(controllers = CircularController.class)
@@ -42,7 +45,7 @@ class CircularControllerTest {
     private CircularMapper circularMapper;
     @SpyBean
     private UseCaseExecutorImpl useCaseExecutor;
-    
+
     @MockBean
     private CreateCircularUseCase createCircularUseCase;
     @MockBean
@@ -59,48 +62,46 @@ class CircularControllerTest {
     }
 
     @Test
-    void create() throws Exception {
-        CircularRequest request = CircularRequest.builder()
-                .withName("a name")
-                .withDescription("the description")
+    void create(@Random String name, @Random String description, @Random String id) throws Exception {
+        // Background
+        CreateCircularUseCase.InputValues input = CreateCircularUseCase.InputValues.builder()
+                .withName(name)
+                .withDescription(description)
                 .build();
-        CircularResponse expected = CircularResponse.builder()
-                .withId("1")
-                .withName("a name")
-                .withDescription("the description")
-                .build();
-
-        CreateCircularUseCase.InputValues in = CreateCircularUseCase.InputValues.builder()
-                .withName("a name")
-                .withDescription("the description")
-                .build();
-        CreateCircularUseCase.OutputValues out = CreateCircularUseCase.OutputValues.builder()
+        CreateCircularUseCase.OutputValues output = CreateCircularUseCase.OutputValues.builder()
                 .withCircular(Circular.builder()
-                        .withId(Identity.of("1"))
-                        .withName("a name")
-                        .withDescription("the description")
+                        .withId(Identity.of(id))
+                        .withName(name)
+                        .withDescription(description)
                         .build()
                 ).build();
-        doReturn(out)
-                .when(createCircularUseCase)
-                .execute(in);
+        doReturn(output).when(createCircularUseCase).execute(eq(input));
 
-        MockHttpServletRequestBuilder content = post("/circular")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request));
+        // Given
+        CircularRequest content = CircularRequest.builder()
+                .withName(name)
+                .withDescription(description)
+                .build();
 
-        RequestBuilder payload = asyncDispatch(mockMvc.perform(content)
+        // When
+        MockHttpServletRequestBuilder request = post("/circular")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(content));
+        RequestBuilder action = asyncDispatch(mockMvc.perform(request)
                 .andExpect(request().asyncStarted())
                 .andReturn());
+        MvcResult actual = mockMvc.perform(action).andReturn();
 
-        mockMvc.perform(payload)
-                .andExpect(status().isCreated())
-                .andExpect(mvcResult -> {
-                    String actual = mvcResult.getResponse().getContentAsString();
+        // Then
+        CircularResponse expected = CircularResponse.builder()
+                .withId(id)
+                .withName(name)
+                .withDescription(description)
+                .build();
 
-                    assertThat(objectMapper.writeValueAsString(expected))
-                            .isEqualToIgnoringWhitespace(actual);
-                });
+        assertThat(actual.getResponse().getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(actual.getResponse().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        assertThat(actual.getResponse().getContentAsString()).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(expected));
     }
 
     @TestConfiguration
