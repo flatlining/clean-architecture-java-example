e1fcd78c46f53e731c34a816d930221bec8e3741
Matias S <mschertel@gmail.com>
2020-04-28 18:40:42 -0300

    add nested test classes

--- presenter/src/test/java/dev/schertel/cq/presenter/rest/circular/CircularControllerTest.java
+++ presenter/src/test/java/dev/schertel/cq/presenter/rest/circular/CircularControllerTest.java
@@ -12,6 +12,7 @@ import dev.schertel.cq.presenter.usecase.UseCaseExecutorImpl;
 import io.github.glytching.junit.extension.random.Random;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,151 +74,160 @@ class CircularControllerTest {
         );
     }
 
-    @Test
-    void postCreateSuccessfully(@Random Circular circular) throws Exception {
-        // Background
-        CreateCircularUseCase.InputValues input = CreateCircularUseCase.InputValues.builder()
-                .withName(circular.getName())
-                .withDescription(circular.getDescription())
-                .build();
-        CreateCircularUseCase.OutputValues output = CreateCircularUseCase.OutputValues.builder()
-                .withCircular(Circular.builder()
-                        .withId(circular.getId())
-                        .withName(circular.getName())
-                        .withDescription(circular.getDescription())
-                        .build())
-                .build();
-        doReturn(output).when(createCircularUseCase).execute(eq(input));
-
-        // Given
-        CircularRequest content = CircularRequest.builder()
-                .withName(circular.getName())
-                .withDescription(circular.getDescription())
-                .build();
-        RequestBuilder request = post("/circular")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(objectMapper.writeValueAsString(content));
-
-        // When
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(request().asyncStarted())
-                .andReturn();
-
-        // Then
-        CircularResponse expected = CircularResponse.builder()
-                .withId(circular.getId().getId())
-                .withName(circular.getName())
-                .withDescription(circular.getDescription())
-                .build();
-
-        mockMvc.perform(asyncDispatch(result))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(actual -> {
-                    assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
-                });
-    }
-
-    @Test
-    void getReadAllSuccessfully(@Random(size = 5, type = Circular.class) List<Circular> circulars) throws Exception {
-        // Background
-        ReadAllCircularUseCase.InputValues input = ReadAllCircularUseCase.InputValues.builder().build();
-        ReadAllCircularUseCase.OutputValues output = ReadAllCircularUseCase.OutputValues.builder()
-                .withCircular(circulars)
-                .build();
-        doReturn(output).when(readAllCircularUseCase).execute(eq(null));
-
-        // Given
-        RequestBuilder request = get("/circular");
-
-        // When
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(request().asyncStarted())
-                .andReturn();
-
-        // Then
-        List<CircularResponse> expected = circulars.stream().map(c -> CircularResponse.builder()
-                .withId(c.getId().getId())
-                .withName(c.getName())
-                .withDescription(c.getDescription()).build()).collect(Collectors.toList());
-
-        mockMvc.perform(asyncDispatch(result))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(actual -> {
-                    assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
-                });
-    }
-
-    @Test
-    void getReadSuccessfully(@Random Circular circular) throws Exception {
-        // Background
-        ReadCircularUseCase.InputValues input = ReadCircularUseCase.InputValues.builder()
-                .withIdentity(circular.getId())
-                .build();
-        ReadCircularUseCase.OutputValues output = ReadCircularUseCase.OutputValues.builder()
-                .withCircular(circular)
-                .build();
-        doReturn(output).when(readCircularUseCase).execute(eq(input));
-
-        // Given
-        RequestBuilder request = get("/circular/{id}", circular.getId().getId());
-
-        // When
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(request().asyncStarted())
-                .andReturn();
-
-        // Then
-        CircularResponse expected = CircularResponse.builder()
-                .withId(circular.getId().getId())
-                .withName(circular.getName())
-                .withDescription(circular.getDescription())
-                .build();
-
-        mockMvc.perform(asyncDispatch(result))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(actual -> {
-                    assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
-                });
-    }
-
-    @Test
-    void getReadNotFound(@Random String id) throws Exception {
-        // Background
-        ReadCircularUseCase.InputValues input = ReadCircularUseCase.InputValues.builder()
-                .withIdentity(Identity.of(id))
-                .build();
-        doThrow(NotFoundException.of(id)).when(readCircularUseCase).execute(eq(input));
-
-        // Given
-        RequestBuilder request = get("/circular/{id}", id);
-
-        // When
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(request().asyncStarted())
-                .andReturn();
-
-        // Then
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
-        ApiResponse expected = ApiResponse.builder()
-                .withTimestamp(null)
-                .withStatus(httpStatus.value())
-                .withReason(httpStatus.getReasonPhrase())
-                .withMessage(id)
-                .build();
-
-        ResultActions resultActions = mockMvc.perform(asyncDispatch(result))
-                .andExpect(status().isNotFound())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(actual -> {
-                    assertThatJson(actual.getResponse().getContentAsString()).whenIgnoringPaths("timestamp").isEqualTo(objectMapper.writeValueAsString(expected));
-                });
-    }
-
     @TestConfiguration
     @ComponentScan(basePackages = {"dev.schertel.cq.presenter.rest.circular"})
     static class Config {
+    }
+
+    @Nested
+    class Read {
+        @Test
+        void readFullSuccessfully(@Random Circular circular) throws Exception {
+            // Background
+            ReadCircularUseCase.InputValues input = ReadCircularUseCase.InputValues.builder()
+                    .withIdentity(circular.getId())
+                    .build();
+            ReadCircularUseCase.OutputValues output = ReadCircularUseCase.OutputValues.builder()
+                    .withCircular(circular)
+                    .build();
+            doReturn(output).when(readCircularUseCase).execute(eq(input));
+
+            // Given
+            RequestBuilder request = get("/circular/{id}", circular.getId().getId());
+
+            // When
+            MvcResult result = mockMvc.perform(request)
+                    .andExpect(request().asyncStarted())
+                    .andReturn();
+
+            // Then
+            CircularResponse expected = CircularResponse.builder()
+                    .withId(circular.getId().getId())
+                    .withName(circular.getName())
+                    .withDescription(circular.getDescription())
+                    .build();
+
+            mockMvc.perform(asyncDispatch(result))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(actual -> {
+                        assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
+                    });
+        }
+
+        @Test
+        void readNotFound(@Random String id) throws Exception {
+            // Background
+            ReadCircularUseCase.InputValues input = ReadCircularUseCase.InputValues.builder()
+                    .withIdentity(Identity.of(id))
+                    .build();
+            doThrow(NotFoundException.of(id)).when(readCircularUseCase).execute(eq(input));
+
+            // Given
+            RequestBuilder request = get("/circular/{id}", id);
+
+            // When
+            MvcResult result = mockMvc.perform(request)
+                    .andExpect(request().asyncStarted())
+                    .andReturn();
+
+            // Then
+            HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+            ApiResponse expected = ApiResponse.builder()
+                    .withTimestamp(null)
+                    .withStatus(httpStatus.value())
+                    .withReason(httpStatus.getReasonPhrase())
+                    .withMessage(id)
+                    .build();
+
+            ResultActions resultActions = mockMvc.perform(asyncDispatch(result))
+                    .andExpect(status().isNotFound())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(actual -> {
+                        assertThatJson(actual.getResponse().getContentAsString()).whenIgnoringPaths("timestamp").isEqualTo(objectMapper.writeValueAsString(expected));
+                    });
+        }
+    }
+
+    @Nested
+    class ReadAll {
+        @Test
+        void readAllSuccessfully(@Random(size = 5, type = Circular.class) List<Circular> circulars) throws Exception {
+            // Background
+            ReadAllCircularUseCase.InputValues input = ReadAllCircularUseCase.InputValues.builder().build();
+            ReadAllCircularUseCase.OutputValues output = ReadAllCircularUseCase.OutputValues.builder()
+                    .withCircular(circulars)
+                    .build();
+            doReturn(output).when(readAllCircularUseCase).execute(eq(null));
+
+            // Given
+            RequestBuilder request = get("/circular");
+
+            // When
+            MvcResult result = mockMvc.perform(request)
+                    .andExpect(request().asyncStarted())
+                    .andReturn();
+
+            // Then
+            List<CircularResponse> expected = circulars.stream().map(c -> CircularResponse.builder()
+                    .withId(c.getId().getId())
+                    .withName(c.getName())
+                    .withDescription(c.getDescription()).build()).collect(Collectors.toList());
+
+            mockMvc.perform(asyncDispatch(result))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(actual -> {
+                        assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
+                    });
+        }
+    }
+
+    @Nested
+    class Create {
+        @Test
+        void createFullSuccessfully(@Random Circular circular) throws Exception {
+            // Background
+            CreateCircularUseCase.InputValues input = CreateCircularUseCase.InputValues.builder()
+                    .withName(circular.getName())
+                    .withDescription(circular.getDescription())
+                    .build();
+            CreateCircularUseCase.OutputValues output = CreateCircularUseCase.OutputValues.builder()
+                    .withCircular(Circular.builder()
+                            .withId(circular.getId())
+                            .withName(circular.getName())
+                            .withDescription(circular.getDescription())
+                            .build())
+                    .build();
+            doReturn(output).when(createCircularUseCase).execute(eq(input));
+
+            // Given
+            CircularRequest content = CircularRequest.builder()
+                    .withName(circular.getName())
+                    .withDescription(circular.getDescription())
+                    .build();
+            RequestBuilder request = post("/circular")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(objectMapper.writeValueAsString(content));
+
+            // When
+            MvcResult result = mockMvc.perform(request)
+                    .andExpect(request().asyncStarted())
+                    .andReturn();
+
+            // Then
+            CircularResponse expected = CircularResponse.builder()
+                    .withId(circular.getId().getId())
+                    .withName(circular.getName())
+                    .withDescription(circular.getDescription())
+                    .build();
+
+            mockMvc.perform(asyncDispatch(result))
+                    .andExpect(status().isCreated())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(actual -> {
+                        assertThatJson(actual.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
+                    });
+        }
     }
 }
