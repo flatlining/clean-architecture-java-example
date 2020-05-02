5a585c8e187d5149ddeaf5f91d333c162b4a0e7f
Matias S <mschertel@gmail.com>
2020-05-02 01:22:25 -0300

    add first unit test for repository

---
+++ data/src/test/java/dev/schertel/cq/data/repository/CircularRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package dev.schertel.cq.data.repository;
+
+import dev.schertel.cq.core.domain.Circular;
+import io.github.glytching.junit.extension.random.Random;
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(RandomBeansExtension.class)
+class CircularRepositoryImplTest {
+    private Logger logger = LoggerFactory.getLogger(CircularRepositoryImplTest.class);
+
+    private CircularRepositoryImpl cut;
+
+    @BeforeEach
+    void setUp() {
+        this.cut = new CircularRepositoryImpl(logger);
+    }
+
+    @Test
+    void create(@Random Circular expected) {
+        assertEquals(expected, cut.create(expected));
+    }
+}
