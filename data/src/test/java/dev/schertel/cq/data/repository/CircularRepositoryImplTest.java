38811ec82e917fc432f05b8ad24e36455f8d4421
Matias S <mschertel@gmail.com>
2020-05-02 01:29:09 -0300

    using mocked logger

--- data/src/test/java/dev/schertel/cq/data/repository/CircularRepositoryImplTest.java
+++ data/src/test/java/dev/schertel/cq/data/repository/CircularRepositoryImplTest.java
@@ -6,6 +6,7 @@ import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(RandomBeansExtension.class)
 class CircularRepositoryImplTest {
-    private Logger logger = LoggerFactory.getLogger(CircularRepositoryImplTest.class);
+    @Mock private Logger logger;
 
     private CircularRepositoryImpl cut;
 
