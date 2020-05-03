292b22845cb5140d0cf83d2153ac0744908d9879
Matias S <mschertel@gmail.com>
2020-05-02 21:34:13 -0300

    add fail to delete

--- core/src/test/java/dev/schertel/cq/core/usecase/circular/DeleteCircularUseCaseTest.java
+++ core/src/test/java/dev/schertel/cq/core/usecase/circular/DeleteCircularUseCaseTest.java
@@ -1,6 +1,8 @@
 package dev.schertel.cq.core.usecase.circular;
 
 import dev.schertel.cq.core.domain.Circular;
+import dev.schertel.cq.core.domain.Identity;
+import dev.schertel.cq.core.domain.NotFoundException;
 import io.github.glytching.junit.extension.random.Random;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,7 +39,7 @@ class DeleteCircularUseCaseTest {
     }
 
     @Test
-    void successDeleteExisting(@Random Circular deleted) {
+    void successToDelete(@Random Circular deleted) {
         // Background
         doReturn(Optional.of(deleted)).when(repository).deleteByIdentity(deleted.getId());
 
@@ -54,5 +57,28 @@ class DeleteCircularUseCaseTest {
         DeleteCircularUseCase.OutputValues expected = outputBuilder.build();
 
         assertThat(actual).isNotNull().isEqualTo(expected);
+    }
+
+    @Test
+    void failToDelete(@Random Identity toDelete) {
+        // Background
+        doReturn(Optional.empty()).when(repository).deleteByIdentity(toDelete);
+
+        // Given
+        DeleteCircularUseCase.InputValues input = inputBuilder
+                .withIdentity(toDelete)
+                .build();
+
+        // When
+        NotFoundException actual = assertThrows(NotFoundException.class, () -> {
+            cut.execute(input);
+        });
+
+        // Then
+        verify(repository, times(1)).deleteByIdentity(toDelete);
+
+        NotFoundException expected = NotFoundException.of(toDelete.getId());
+
+        assertThat(actual).isNotNull().isEqualToComparingFieldByFieldRecursively(expected);
     }
 }
