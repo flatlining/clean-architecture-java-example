2c8ffb3d81dc905fff26e78ec8315c4954879e43
Matias S <mschertel@gmail.com>
2020-05-02 22:04:11 -0300

    add read all from non empty repository

--- core/src/test/java/dev/schertel/cq/core/usecase/circular/ReadAllCircularUseCaseTest.java
+++ core/src/test/java/dev/schertel/cq/core/usecase/circular/ReadAllCircularUseCaseTest.java
@@ -1,5 +1,7 @@
 package dev.schertel.cq.core.usecase.circular;
 
+import dev.schertel.cq.core.domain.Circular;
+import io.github.glytching.junit.extension.random.Random;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -49,6 +52,27 @@ class ReadAllCircularUseCaseTest {
 
         ReadAllCircularUseCase.OutputValues expected = outputBuilder
                 .withCircular(Collections.emptyList())
+                .build();
+
+        assertThat(actual).isNotNull().isEqualTo(expected);
+    }
+
+    @Test
+    void readAllNonEmptyRepository(@Random(size = 5, type = Circular.class) List<Circular> toRead) {
+        // Background
+        doReturn(toRead).when(repository).readAll();
+
+        // Given
+        ReadAllCircularUseCase.InputValues input = inputBuilder.build();
+
+        // When
+        ReadAllCircularUseCase.OutputValues actual = cut.execute(input);
+
+        // Then
+        verify(repository, times(1)).readAll();
+
+        ReadAllCircularUseCase.OutputValues expected = outputBuilder
+                .withCircular(toRead)
                 .build();
 
         assertThat(actual).isNotNull().isEqualTo(expected);
