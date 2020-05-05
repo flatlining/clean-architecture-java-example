063bb6e07b4944516a982b7ea39f5b9f4248662a
Matias S <mschertel@gmail.com>
2020-05-05 00:57:28 -0300

    add constants

--- config/src/test/java/karate-config.js
+++ config/src/test/java/karate-config.js
@@ -1,27 +1,32 @@
 function fn() {
+    var APP_HTTPS = 'app.server.https';
+    var APP_DOMAIN = 'app.server.domain';
+    var APP_PORT = 'app.server.port';
+    var APP_BASE_URL = 'app.server.baseUrl';
+
     var env = karate.env; // get java system property 'karate.env'
     karate.log('karate.env system property was:', env);
     if (!env) {
         env = 'dev'; // a custom 'intelligent' default
     }
 
-    var port = karate.properties['app.server.port'];
+    var port = karate.properties[APP_PORT];
     if (!port) {
         port = 8080;
     }
 
-    var domain = karate.properties['app.server.domain'];
+    var domain = karate.properties[APP_DOMAIN];
     if (!domain) {
         domain = 'localhost';
     }
 
     var protocol = 'http';
-    if (karate.properties['app.server.https'] == 'true') {
+    if (karate.properties[APP_HTTPS] == 'true') {
         protocol = 'https';
         karate.configure('ssl', true);
     }
 
-    var baseUrl = karate.properties['app.server.baseUrl'];
+    var baseUrl = karate.properties[APP_BASE_URL];
     if (!baseUrl) {
         baseUrl = protocol + '://' + domain + ':' + port;
     }
