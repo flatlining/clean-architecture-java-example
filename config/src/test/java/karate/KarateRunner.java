ab64ba9fe88d6424800ebf98da3a9f725bbc1dea
Matias S <mschertel@gmail.com>
2020-05-04 23:30:37 -0300

    add ignore tag support

--- config/src/test/java/karate/KarateRunner.java
+++ config/src/test/java/karate/KarateRunner.java
@@ -5,6 +5,6 @@ import com.intuit.karate.junit5.Karate;
 class KarateRunner {
     @Karate.Test
     Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+        return Karate.run().tags("~@ignore").relativeTo(getClass());
     }
 }
