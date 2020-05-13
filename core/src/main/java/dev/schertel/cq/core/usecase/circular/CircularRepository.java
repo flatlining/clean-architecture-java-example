fa7d8be6f26d46bd94c16dfa2f7c37f0757d6e40
Matias S <matias.schertel@sap.com>
2020-05-13 13:34:55 -0300

    document repository methods

--- core/src/main/java/dev/schertel/cq/core/usecase/circular/CircularRepository.java
+++ core/src/main/java/dev/schertel/cq/core/usecase/circular/CircularRepository.java
@@ -7,13 +7,41 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CircularRepository {
+    /**
+     * Create a {@link Circular}
+     *
+     * @param circular The {@link Circular} to create
+     * @return The {@link Circular} created
+     */
     Circular create(Circular circular);
 
+    /**
+     * Read all existing {@link Circular}
+     *
+     * @return A list of read {@link Circular}, empty list if none exist
+     */
     List<Circular> readAll();
 
+    /**
+     * Read a {@link Circular} by {@link Identity}
+     *
+     * @param identity The {@link Identity} to read the {@link Circular}
+     * @return The {@link Circular} found for {@link Identity}, empty {@link Optional} if none exist
+     */
     Optional<Circular> readByIdentity(Identity identity);
 
+    /**
+     * Delete all existing {@link Circular}
+     *
+     * @return A list of deleted {@link Circular}, empty list if none deleted
+     */
     List<Circular> deleteAll();
 
+    /**
+     * Delete a {@link Circular} by {@link Identity}
+     *
+     * @param identity The {@link Identity} to delete the {@link Circular}
+     * @return The {@link Circular} deleted for {@link Identity}, empty {@link Optional} if none deleted
+     */
     Optional<Circular> deleteByIdentity(Identity identity);
 }
