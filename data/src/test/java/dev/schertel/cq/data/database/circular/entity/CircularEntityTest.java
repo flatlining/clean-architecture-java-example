52db7aefed7ce420cedf7305c91783de0b2e387f
Matias S <matias.schertel@sap.com>
2020-05-14 19:25:21 -0300

    add builder test

--- data/src/test/java/dev/schertel/cq/data/database/circular/entity/CircularEntityTest.java
+++ data/src/test/java/dev/schertel/cq/data/database/circular/entity/CircularEntityTest.java
@@ -2,17 +2,63 @@ package dev.schertel.cq.data.database.circular.entity;
 
 import com.jparams.verifier.tostring.NameStyle;
 import com.jparams.verifier.tostring.ToStringVerifier;
+import io.github.glytching.junit.extension.random.Random;
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(RandomBeansExtension.class)
 class CircularEntityTest {
     private final Class<CircularEntity> CLAZZ = CircularEntity.class;
 
+    private CircularEntity.Builder cut;
+
     @BeforeEach
     void setUp() {
+        this.cut = CircularEntity.builder();
+    }
+
+    @Nested
+    class Builder {
+        @Test
+        void nullObject() {
+            // Given
+
+            // When
+            CircularEntity actual = cut.build();
+
+            // Then
+            assertThat(actual).isNotNull().satisfies(circularEntity -> {
+                assertThat(circularEntity.getId()).isNull();
+                assertThat(circularEntity.getName()).isNull();
+                assertThat(circularEntity.getDescription()).isNull();
+            });
+        }
+
+        @Test
+        void fullObject(@Random String id, @Random String name, @Random String description) {
+            // Given
+            cut
+                    .withId(id)
+                    .withName(name)
+                    .withDescription(description);
+
+            // When
+            CircularEntity actual = cut.build();
+
+            // Then
+            assertThat(actual).isNotNull().satisfies(circularEntity -> {
+                assertThat(circularEntity.getId()).isEqualTo(id);
+                assertThat(circularEntity.getName()).isEqualTo(name);
+                assertThat(circularEntity.getDescription()).isEqualTo(description);
+            });
+        }
     }
 
     @Nested
